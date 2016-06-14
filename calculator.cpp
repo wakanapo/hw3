40f2d13ec9ce80c68784f30a0a6a3787cd5b72ce
wakanapo <wakana.tn16@gmail.com>
2016-06-14 20:51:44 +0900

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -118,7 +118,6 @@ Formula* buildSyntaxTree(std::vector<std::string>* token_vector) {
   Formula* formula = new Formula(NUMBER, 0);
   std::string::size_type index = 0;
   while (index < (*token_vector).size()) {
-    std::cout << (*token_vector)[index] <<std::endl;
     if ((*token_vector)[index] == "(") {
       std::string ope = (*token_vector)[index-1];
       (*token_vector).erase((*token_vector).begin(), (*token_vector).begin() + index + 1);
